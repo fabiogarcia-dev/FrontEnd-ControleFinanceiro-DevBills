40b3ef288379cec828bbac35c399b944b10302f6
fabiogarcia-dev <fabio.ti.am@gmail.com>
2025-09-30 16:48:32 -0400

    Projeto de estudo FrontEnd controle financeiro, montagem da estrutura em TS

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import Button from "../components/Button"
+
+const Home =() => {
+    return(
+        <div>
+            <h2>Home</h2>
+            <Button variant="outline" isLoading>Clique aqui</Button>
+        </div>
+    );
+};
+
+export default Home;
